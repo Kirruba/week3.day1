f2d6194feff139b703048f99b3ec0327ff6a9194
Kirruba <112795574+Kirruba@users.noreply.github.com>
2022-09-18 11:29:25 +0530

    Add files via upload

---
+++ Overriding/AxisBank.java
@@ -0,0 +1,7 @@
+package week3.day1;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+		System.out.println("Overriden Deposit amount in AxisBank is Rs.5,000");
+	}
+}
---
+++ Overriding/MyBank.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+public class MyBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BankInfo BI = new BankInfo();
+		BI.saving();
+		BI.fixed();
+		BI.deposit();
+		AxisBank AB = new AxisBank();
+		AB.deposit();
+	}
+
+}
