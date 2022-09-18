43cee7395a12361d372f9824a5cc9abb1fca9b4e
Kirruba <112795574+Kirruba@users.noreply.github.com>
2022-09-18 11:28:40 +0530

    Create BankInfo.java

---
+++ Overriding/BankInfo.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("Saving Account Balance is Rs.10,000");
+	}
+
+	public void fixed() {
+		System.out.println("Fixed Deposit for 2 years");
+	}
+
+	public void deposit() {
+		System.out.println("Deposited amount is Rs.5,000");
+	}
+}
