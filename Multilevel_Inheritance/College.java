4a57c197b95f90d474d29774c82a8714cca1dfd2
Kirruba <112795574+Kirruba@users.noreply.github.com>
2022-09-18 11:25:41 +0530

    Create College.java

---
+++ Multilevel_Inheritance/College.java
@@ -0,0 +1,18 @@
+package org.college;
+
+public class College {
+	
+	public void collegeName() {
+		System.out.println("College Name:RajaLakshmi Institute of Technology");
+		
+	}
+	public void collegeCode() {
+		System.out.println("College Code:2117");
+	}
+	public void collegeRank() {
+		System.out.println("College Rank:12");
+		
+	}
+
+
+}
