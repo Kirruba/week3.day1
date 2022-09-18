2da6c6861dbf78b27a1fde9389488ac02bf6f489
Kirruba <112795574+Kirruba@users.noreply.github.com>
2022-09-18 11:33:36 +0530

    Add files via upload

---
+++ Overloading/MyStudent.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class MyStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Students S = new Students();
+		S.getStudentInfo(201301090);
+		S.getStudentInfo(201301090, "Kirruba");
+		S.getStudentInfo("abc@gmail.com", "9876012345");
+	}
+
+}
